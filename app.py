7a70d69e0515aa724cf6941db13166929929c9cf
sat4git <31431234+sat4git@users.noreply.github.com>
2023-01-26 05:04:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ while True:
         with fig_col1:
             st.markdown("### First Chart")
             fig = px.bar(data, x=data["id"].astype("str")+"_"+data["CryptoCoin"], y="Profit_Loss_percentage",
-                        color=["red", "green","red", "green","red"], color_discrete_map="identity",
+                        color=['red' if i<0 else 'green' for i in data['Profit_Loss_percentage']], color_discrete_map="identity",
                         category_orders={"x": data["id"].astype("str")+"_"+data["CryptoCoin"]}
                         )
             #['red' if i>0 else 'green' for i in data['Profit_Loss_percentage'].astype("float")]

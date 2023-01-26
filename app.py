6866ff9963388db3eb011fd1dfc1a8dccffdbcd8
sat4git <31431234+sat4git@users.noreply.github.com>
2023-01-26 02:52:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -101,6 +101,7 @@ while True:
         st.markdown("### Detailed Data View")
         sql = "select Date, Time, Bitcoin_EUR, CryptoCoin, Quantity, BuyPrice, SellPrice, Profit_Loss_percentage, Bitcoin_diff, Profit_after_fees from TradeBook"
         data = pd.read_sql(sql,con=my_conn)
+        data['Time'] = data['Time'].astype(str) 
         st.dataframe(data.style.applymap(color_Profit, subset=['Profit_Loss_percentage','Bitcoin_diff','Profit_after_fees']))
         time.sleep(10)
     #placeholder.empty()
